41a343403c08d5b594d47bedf77857bb2e9d8742
Alex Doss <alex.doss42@gmail.com>
2020-06-17 23:52:34 -0600

    builds a basic promise for demo

--- promise.js
+++ promise.js
@@ -1,4 +1,10 @@
 
 const p = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve(1);
+  }, 2000);
   
+  // reject(new Error('message'));
 });
+
+p.then(result => console.log("Result: ", result));
